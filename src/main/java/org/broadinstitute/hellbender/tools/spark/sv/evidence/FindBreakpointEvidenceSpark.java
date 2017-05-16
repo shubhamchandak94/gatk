5aa6280cbbe0407b21f961ff63ac08be170d207d
vruano <valentin.ruano@gmail.com>
2017-05-16 09:23:49 -0400

    Work in progress

--- src/main/java/org/broadinstitute/hellbender/tools/spark/sv/evidence/FindBreakpointEvidenceSpark.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/sv/evidence/FindBreakpointEvidenceSpark.java
@@ -37,6 +37,7 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.broadinstitute.hellbender.tools.spark.sv.StructuralVariationDiscoveryArgumentCollection.FindBreakpointEvidenceSparkArgumentCollection;
 import static org.broadinstitute.hellbender.tools.spark.sv.evidence.BreakpointEvidence.ReadEvidence;
@@ -349,7 +350,6 @@ public final class FindBreakpointEvidenceSpark extends GATKSparkTool {
         @Override
         public AlignedAssemblyOrExcuse apply( final Tuple2<Integer, List<SVFastqUtils.FastqRead>> intervalAndReads ) {
             final List<SVFastqUtils.FastqRead> readsList = intervalAndReads._2();
-
             final int fastqSize = readsList.stream().mapToInt(FastqRead -> FastqRead.getBases().length).sum();
             if (fastqSize > maxFastqSize) {
                 return new AlignedAssemblyOrExcuse(intervalAndReads._1(),
