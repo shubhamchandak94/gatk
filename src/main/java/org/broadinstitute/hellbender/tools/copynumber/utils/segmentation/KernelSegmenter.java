edc9feeb0ab6b8e181ccb7b75982ade39a62739d
Samuel Lee <slee@broadinstitute.org>
2017-08-30 16:04:53 -0400

    calculateSegmentCost and javadoc

--- src/main/java/org/broadinstitute/hellbender/tools/copynumber/utils/segmentation/KernelSegmenter.java
+++ src/main/java/org/broadinstitute/hellbender/tools/copynumber/utils/segmentation/KernelSegmenter.java
@@ -1,5 +1,6 @@
 package org.broadinstitute.hellbender.tools.copynumber.utils.segmentation;
 
+import com.netflix.servo.util.Iterables;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.commons.math3.random.RandomGeneratorFactory;
@@ -8,14 +9,21 @@ import org.broadinstitute.hellbender.utils.param.ParamUtils;
 
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
+ * <p>
  * Segment data (i.e., find multiple changepoints) using a method based on the kernel-segmentation algorithm
- * described in <a href="https://hal.inria.fr/hal-01413230/document">https://hal.inria.fr/hal-01413230/document</a>, which gives a framework to quickly calculate the cost of
- * a segment given a low-rank approximation to a specified kernel.  However, unlike the algorithm described there,
- * which seeks to minimize a global segmentation cost, the method implemented here instead finds candidate changepoints
- * based on the local costs within windows of various sizes.
+ * described in <a href="https://hal.inria.fr/hal-01413230/document">https://hal.inria.fr/hal-01413230/document</a>,
+ * which gives a framework to quickly calculate the cost of a segment given a low-rank approximation to a specified kernel.
+ * However, unlike the algorithm described there, which seeks to minimize a global segmentation cost,
+ * the method implemented here instead finds candidate changepoints based on the local costs within
+ * windows of various sizes.
+ * </p>
  *
+ * <p>
  * Given <i>N</i> data points to segment, the basic steps of the method are:
  *
  * <ol>
@@ -23,12 +31,12 @@ import java.util.function.BiFunction;
  * </ol>
  * <ol>
  *     2) Select a kernel (linear for sensitivity to changes in the distribution mean,
- *     Gaussian with a specified variance <i>σ<sup>2</sup></i> for multimodal data, etc.)
+ *     Gaussian with a specified variance for multimodal data, etc.)
  *     and a subsample of <i>p</i> points to approximate it using singular value decomposition.
  * </ol>
  * <ol>
- *     3) Select window sizes <i>w<sub>j</sub></i> for which to compute local costs at each point.
- *     To be precise, we compute the cost of a changepoint at the point with index <i>i</i>,
+ *     3) Select window sizes <i>w<sub>j</sub></i> for which to calculate local costs at each point.
+ *     To be precise, we calculate the cost of a changepoint at the point with index <i>i</i>,
  *     assuming adjacent segments containing the points with indices <i>[i - w<sub>j</sub> + 1, i]</i>
  *     and <i>[i + 1, i + w<sub>j</sub>]</i>.
  * </ol>
@@ -36,12 +44,12 @@ import java.util.function.BiFunction;
  *     4) For each of these cost functions, find (up to) the  <i>C<sub>max</sub></i> most significant local minima.
  *     The problem of finding local minima of a noisy function can be solved by using topological persistence
  *     (e.g., <a href="https://people.mpi-inf.mpg.de/~weinkauf/notes/persistence1d.html">https://people.mpi-inf.mpg.de/~weinkauf/notes/persistence1d.html</a>
- *     and <a href="http://www2.iap.fr/users/sousbie/web/html/indexd3dd.html?post/Persistence-and-simplification>http://www2.iap.fr/users/sousbie/web/html/indexd3dd.html?post/Persistence-and-simplification</a>).
+ *     and <a href="http://www2.iap.fr/users/sousbie/web/html/indexd3dd.html?post/Persistence-and-simplification">http://www2.iap.fr/users/sousbie/web/html/indexd3dd.html?post/Persistence-and-simplification</a>).
  * </ol>
  * <ol>
  *     5) These sets of local minima from all window sizes together provide the pool of candidate changepoints
  *     (some of which may overlap exactly or approximately).  We perform backwards selection using the global segmentation cost.
- *     That is, we compute the global segmentation cost given all the candidate changepoints,
+ *     That is, we calculate the global segmentation cost given all the candidate changepoints,
  *     calculate the cost change for removing each of the changepoints individually,
  *     remove the changepoint with the minimum cost change, and repeat.
  *     This gives the global cost as a function of the number of changepoints <i>C</i>.
@@ -51,9 +59,16 @@ import java.util.function.BiFunction;
  *     number of changepoints, where <i>A</i> and <i>B</i> are specified penalty factors.
  *
  * </ol>
+ * </p>
  *
+ * <p>
  * See discussion at <a href="https://github.com/broadinstitute/gatk/issues/2858#issuecomment-324125586">https://github.com/broadinstitute/gatk/issues/2858#issuecomment-324125586</a>
  * and accompanying plots for more detail.
+ * </p>
+ *
+ * <p>
+ * Note that we break with camelCase naming convention in places to match some notation in the paper
+ * </p>
  *
  * @author Samuel Lee &lt;slee@broadinstitute.org&gt;
  */
@@ -86,8 +101,8 @@ public final class KernelSegmenter<T> {
         }
 
         final RandomGenerator rng = RandomGeneratorFactory.createRandomGenerator(new Random(RANDOM_SEED));
-        final RealMatrix reducedObservationMatrix = computeReducedObservationMatrix(rng, data, kernel, kernelApproximationDimension);
-        final double[] kernelApproximationDiagonal = computeKernelApproximationDiagonal(reducedObservationMatrix);
+        final RealMatrix reducedObservationMatrix = calculateReducedObservationMatrix(rng, data, kernel, kernelApproximationDimension);
+        final double[] kernelApproximationDiagonal = calculateKernelApproximationDiagonal(reducedObservationMatrix);
         final List<Integer> changepointCandidates = findChangepointCandidates(
                 data, reducedObservationMatrix, kernelApproximationDiagonal, maxNumChangepoints, windowSizes);
         return selectChangepoints(
@@ -96,8 +111,8 @@ public final class KernelSegmenter<T> {
     }
 
     private final class Segment {
-        private final int start;    //index of start point, inclusive
-        private final int end;      //index of end point, inclusive
+        private final int start;    //inclusive index of start point
+        private final int end;      //inclusive index of end point
         private final double cost;
 
         private Segment(final int start,
@@ -112,18 +127,36 @@ public final class KernelSegmenter<T> {
                         final int end,
                         final RealMatrix reducedObservationMatrix,
                         final double[] kernelApproximationDiagonal) {
-            this(start, end, computeSegmentCost(start, end, reducedObservationMatrix, kernelApproximationDiagonal));
+            this(start, end, calculateSegmentCost(start, end, reducedObservationMatrix, kernelApproximationDiagonal).C);
         }
     }
 
-    private static <T> RealMatrix computeReducedObservationMatrix(final RandomGenerator rng,
-                                                                  final List<T> data,
-                                                                  final BiFunction<T, T, Double> kernel,
-                                                                  final double kernelApproximationDimension) {
+    //represents some quantities used to calculate segment costs iteratively
+    private static final class Cost {
+        private final double D;     //diagonal term
+        private final double[] W;   //intermediate quantity for calculating off-diagonal terms
+        private final double V;     //intermediate quantity for calculating off-diagonal terms
+        private final double C;     //total cost
+
+        private Cost(final double D,
+                     final double[] W,
+                     final double V,
+                     final double C) {
+            this.D = D;
+            this.W = W;
+            this.V = V;
+            this.C = C;
+        }
+    }
+
+    private static <T> RealMatrix calculateReducedObservationMatrix(final RandomGenerator rng,
+                                                                    final List<T> data,
+                                                                    final BiFunction<T, T, Double> kernel,
+                                                                    final double kernelApproximationDimension) {
 
     }
 
-    private static double[] computeKernelApproximationDiagonal(final RealMatrix reducedObservationMatrix) {
+    private static double[] calculateKernelApproximationDiagonal(final RealMatrix reducedObservationMatrix) {
 
     }
 
@@ -144,9 +177,10 @@ public final class KernelSegmenter<T> {
     }
 
     /**
-     * Compute the cost of a segment.  This is defined by Eq. 11 of <a href="https://hal.inria.fr/hal-01413230/document">https://hal.inria.fr/hal-01413230/document</a>
+     * Calculate the cost of a segment.  This is defined by Eq. 11 of
+     * <a href="https://hal.inria.fr/hal-01413230/document">https://hal.inria.fr/hal-01413230/document</a>
      * (except we use the low-rank approximation to the kernel, as described in Sec. 3.2, ibid).
-     * Various recursion relations are used to compute costs iteratively.
+     * Various recurrence relations are used to calculate costs iteratively.
      * @param start inclusive start index of segment
      * @param end   inclusive end index of segment
      * @param reducedObservationMatrix      N x p matrix of projected observations, where N is the number of data points
@@ -154,16 +188,55 @@ public final class KernelSegmenter<T> {
      *                                      this is the Z matrix described in the text preceding Eq. 14, ibid
      * @param kernelApproximationDiagonal   N diagonal terms of the low-rank approximation to the kernel matrix
      */
-    private static double computeSegmentCost(final int start,
+    private static Cost calculateSegmentCost(final int start,
                                              final int end,
                                              final RealMatrix reducedObservationMatrix,
                                              final double[] kernelApproximationDiagonal) {
+        final int N = reducedObservationMatrix.getRowDimension();
+        final int p = reducedObservationMatrix.getColumnDimension();
 
+        //initialize quantities for recurrence
+        double D = kernelApproximationDiagonal[start];
+        double[] W = Arrays.copyOf(reducedObservationMatrix.getRow(start), p);
+        double V = Arrays.stream(W).map(w -> w * w).sum();
+
+        //generate indices for iteration; we need to wrap around to beginning of data if start > end
+        final List<Integer> indices = start <= end 
+                ? IntStream.range(start + 1, end + 1).boxed().collect(Collectors.toList()) 
+                : IntStream.concat(IntStream.range(start + 1, N), IntStream.range(0, end + 1)).boxed().collect(Collectors.toList());
+        
+        //use recurrence relations to iteratively calculate cost
+        for (final int tauPrime : indices) {
+            D += kernelApproximationDiagonal[tauPrime];
+            double ZdotW = 0.;
+            for (int j = 0; j < p; j++) {
+                ZdotW += reducedObservationMatrix.getEntry(tauPrime, j) * W[j];
+                W[j] += reducedObservationMatrix.getEntry(tauPrime, j);
+            }
+            V += 2. * ZdotW + kernelApproximationDiagonal[tauPrime];
+        }
+        final double C = D - V / indices.size();
+
+        return new Cost(D, W, V, C);
     }
 
-    private static double[] computeWindowCosts(final RealMatrix reducedObservationMatrix,
-                                               final double[] kernelApproximationDiagonal,
-                                               final int windowSize) {
+    /**
+     * Calculate the local costs at each point for a given window size <i>w</i>.  Using Eq. 11 of
+     * <a href="https://hal.inria.fr/hal-01413230/document">https://hal.inria.fr/hal-01413230/document</a>
+     * (except we use the low-rank approximation to the kernel, as described in Sec. 3.2, ibid), for each point
+     * indexed by <i>i</i>, we calculate the cost of it being a changepoint with two flanking segments that
+     * contain the points with indices <i>[i - w + 1, i]</i> and <i>[i + 1, i + w]</i>, respectively, and
+     * subtract the cost of a single segment containing all of these points.
+     * Various recurrence relations are used to calculate costs iteratively.
+     * @param reducedObservationMatrix      N x p matrix of projected observations, where N is the number of data points
+     *                                      and p is the dimension of the low-rank approximation of the kernel matrix;
+     *                                      this is the Z matrix described in the text preceding Eq. 14, ibid
+     * @param kernelApproximationDiagonal   N diagonal terms of the low-rank approximation to the kernel matrix
+     * @param windowSize                    number of points to include in either flanking segment when calculating cost
+     */
+    private static double[] calculateWindowCosts(final RealMatrix reducedObservationMatrix,
+                                                 final double[] kernelApproximationDiagonal,
+                                                 final int windowSize) {
 
     }
 }
